6247895a50799cd6859b9a94f923aac8cc7f500f
SamdishaB <samdisha.basra@ryerson.ca>
2022-03-30 14:07:30 -0400

    Deleting from branch to upload issueCreation site

--- pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from .views import HomePageView,AboutPageView, ContactPageView
-
-urlpatterns = [
-    path('', HomePageView, name='home'),
-    path('about/', AboutPageView, name='about'), 
-    path('contact/',ContactPageView, name='contact'), 
-
-]
